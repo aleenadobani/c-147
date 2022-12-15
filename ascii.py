1826f45068b0fa07f48b7a16412292739b97d009
aleenadobani <98019373+aleenadobani@users.noreply.github.com>
2022-12-15 20:11:56 +0530

    Add files via upload

---
+++ ascii.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from tKinter import *
+
+root=Tk()
+root.title("Ascii")
+
+root.geometry("400x400")
+root.configure(background = 'snow')
+
+enter_word = Entry(root)
+enter_word.place(relx=0.5,rely=0.4,anchor=CENTER)
+
+label =Label(root,text = "name in Ascii :", bg="light yellow", fg="black")
+
+def asciiConverter():
+    input_word = enter_word.get()
+    
+    for letter in input_word :
+        label["text"] += str(ord(letter)) + " "
+        
+btn=Button(root,text="show name in Ascii",command=asciiConverter,bg='gold',fg = 'black')
+btn.place(relx=0.5,rely=0.5,anchor=CENTER)
+
+label.place(relx=0.5,rely=0.6,anchor=CENTER)
+
+root.mainloop()
